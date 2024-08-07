e599c140de7f8008ccb5acf969766200489db356
Gustavo Stephano <38302193+zStephano@users.noreply.github.com>
2024-08-06 21:00:19 -0300

    Add Blazor Server study-project to this repo

---
+++ BlazingShop/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using BlazingShop.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazingShop.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Product> Products { get; set; } = null!;
+    public DbSet<Category> Categories { get; set; } = null!;
+}
